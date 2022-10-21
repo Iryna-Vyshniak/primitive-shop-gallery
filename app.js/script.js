6dbd39976361876ffab5d349b68c32ccc59ff479
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-22 01:02:35 +0300

    update files

--- app.js/script.js
+++ app.js/script.js
@@ -1,5 +1,5 @@
 // OOP
-
+/*
 const GoodsList = {
   products: [
     {
@@ -190,8 +190,8 @@ const GoodsList = {
 };
 
 GoodsList.render();
+*/
 
-/*
 class GoodItem {
   id = '';
   title = 'default title';
@@ -199,9 +199,131 @@ class GoodItem {
   alt = '';
   width = 0;
   price = 0;
+
+  constructor(id, title, img, alt, width, price) {
+    this.id = id;
+    this.title = title;
+    this.image = img;
+    this.alt = alt;
+    this.width = width;
+    this.price = price;
+  }
 }
 
 const good = new GoodItem();
 
 console.log(good);
-*/
+
+class GoodList {
+  products = [
+    new GoodItem(1, 'Exercitation ex minim', './img/climbing-min.jpg', 'Climbing', 400, 200),
+    new GoodItem(2, 'Aliquip nulla consectetur', './img/sea-min.jpg', 'Sea', 400, 300),
+    new GoodItem(3,'Laboris dolore proident','./img/photographer-min.jpg', 'Photographer', 400, 250),
+    new GoodItem(4, 'Officia eiusmod proident', './img/thin-min.jpg', 'Thin', 400, 150),
+    new GoodItem(5, 'Duis veniam esse voluptate', './img/alessio-min.jpg', 'Alessio', 400, 550),
+    new GoodItem(6, 'Deserunt consequat proident', './img/boat-min.jpg', 'Boat', 400, 250),
+    new GoodItem(7, 'Do sint ad sint nulla', './img/cuevas-min.jpg', 'Cuevas', 400, 350),
+    new GoodItem(8, 'Deserunt consequat proident', './img/friends-min.jpg', 'Friends', 400, 550),
+    new GoodItem(9, 'Do aliquip culpa', './img/girl-min.jpg', 'Sea', 400, 750),
+    new GoodItem(10, 'Dexercitationure occaecat', './img/ireland-min.jpg', 'Island', 400, 250),
+    new GoodItem(11, 'Nostrud eiusmod', './img/mount-min.jpg', 'Mount', 400, 550),
+    new GoodItem(11, 'Velit officia qui dolor', './img/street-min.jpg', 'Street', 400, 250),
+    new GoodItem(13, 'Duis veniam esse voluptate', './img/train-min.jpg', 'Train', 400, 150),
+    new GoodItem(14, 'Tempor magna ad consectetur', './img/water-house-min.jpg', 'Water house', 400, 250),
+    new GoodItem(15, 'Reprehenderit', './img/bridge-min.jpg', 'Bridge', 400, 650),
+    new GoodItem(16, 'Enim ipsum mollit', './img/hot-girl-min.jpg', 'China', 400, 750),
+  ];
+
+  constructor() {}
+
+  // show these products in html in #id gallery
+  render() {
+    //   <ul></ul>
+    const parentSet = document.getElementById('gallery');
+
+    for (const item of this.products) {
+    let renderedGood = new RenderedGood(item.id, item.link, item.thumb, item.image, item.descr, item.title, item.price, item.btn);
+    
+    const renderedElement = renderedGood.render();
+      // ul + li...
+      parentSet.appendChild(renderedElement);
+    }
+  }
+}
+
+class RenderedGood {
+
+    constructor(id, link, thumb, image, descr, title, price, btn){
+    this.id = id;
+    this.link = link;
+    this.thumb = thumb;
+    this.image = image;
+    this.descr = descr;
+    this.title = title;
+    this.price = price;
+    this.btn = btn;
+    
+    }
+ render(){
+//  <li>...</li>
+let element = document.createElement('li');
+
+element.id = 'good-' + this.id;
+element.className = 'gallery__item';
+
+let itemLink = document.createElement('a');
+itemLink.setAttribute('href', '#');
+itemLink.classList.add('gallery__item-link');
+
+let thumbImg = document.createElement('div');
+thumbImg.classList.add('gallery__item-thumb');
+
+let img = document.createElement('img');
+img.className = 'gallery__item-img';
+img.src = this.image;
+img.alt = this.alt;
+img.width = this.width;
+
+let elementBlock = document.createElement('div');
+elementBlock.classList.add('gallery__item-descr');
+
+let title = document.createElement('h2');
+title.textContent = this.title;
+title.className = 'gallery__item-title title';
+
+let price = document.createElement('p');
+price.textContent = this.price + '$';
+price.className = 'gallery__item-price';
+
+let button = document.createElement('button');
+button.textContent = 'Buy';
+button.className = 'buy-btn btn';
+
+button.addEventListener('click', () => {} );
+
+//   li > img + div > title + price + btn
+thumbImg.appendChild(img);
+
+elementBlock.appendChild(title);
+elementBlock.appendChild(price);
+elementBlock.appendChild(button);
+
+itemLink.appendChild(thumbImg);
+itemLink.appendChild(elementBlock);
+element.appendChild(itemLink);
+
+return element;
+}
+
+}
+
+
+class Cart {
+    constructor(){
+    
+    }
+}
+
+const goodList = new GoodList();
+
+goodList.render();
