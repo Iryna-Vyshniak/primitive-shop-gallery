1515b3924c0f60f901cfd18974d2b435a578b1df
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-22 20:13:18 +0300

    refactored with class, extend, super, #

--- app.js/script.js
+++ app.js/script.js
@@ -192,31 +192,29 @@ const GoodsList = {
 GoodsList.render();
 */
 
-class Component{
+class Component {
   myFavouriteMovie = 'Exercitation ex minim';
-constructor(id){
-  this.parentElementId = id;
-}
+  constructor(id) {
+    this.parentElementId = id;
+  }
 
-createRenderedElement(tag, className, attributes, content){
-let element = document.createElement(tag);
-element.className = className;
+  createRenderedElement(tag, className, attributes, content) {
+    let element = document.createElement(tag);
+    element.className = className;
 
-if (attributes){
-    for (const attribute of attributes){
-    element.setAttribute(attribute.name, attribute.value)
+    if (attributes) {
+      for (const attribute of attributes) {
+        element.setAttribute(attribute.name, attribute.value);
+      }
+    }
+    if (content) element.innerHTML = content;
+    return element;
   }
 }
-if(content) element.innerHTML = content;
-return element;
-}
-
-}
-
 
 class GoodItem {
-shopName = 'My Movie Shop';
-category = 'Movies';
+  shopName = 'My Movie Shop';
+  category = 'Movies';
 
   id = '';
   title = 'default title';
@@ -236,12 +234,19 @@ category = 'Movies';
 }
 
 class GoodList {
-shopName = 'My Movie Shop';
+  shopName = 'My Movie Shop';
 
   products = [
     new GoodItem(1, 'Exercitation ex minim', './img/climbing-min.jpg', 'Climbing', 400, 200),
     new GoodItem(2, 'Aliquip nulla consectetur', './img/sea-min.jpg', 'Sea', 400, 300),
-    new GoodItem(3,'Laboris dolore proident','./img/photographer-min.jpg', 'Photographer', 400, 250),
+    new GoodItem(
+      3,
+      'Laboris dolore proident',
+      './img/photographer-min.jpg',
+      'Photographer',
+      400,
+      250
+    ),
     new GoodItem(4, 'Officia eiusmod proident', './img/thin-min.jpg', 'Thin', 400, 150),
     new GoodItem(5, 'Duis veniam esse voluptate', './img/alessio-min.jpg', 'Alessio', 400, 550),
     new GoodItem(6, 'Deserunt consequat proident', './img/boat-min.jpg', 'Boat', 400, 250),
@@ -252,7 +257,14 @@ shopName = 'My Movie Shop';
     new GoodItem(11, 'Nostrud eiusmod', './img/mount-min.jpg', 'Mount', 400, 550),
     new GoodItem(11, 'Velit officia qui dolor', './img/street-min.jpg', 'Street', 400, 250),
     new GoodItem(13, 'Duis veniam esse voluptate', './img/train-min.jpg', 'Train', 400, 150),
-    new GoodItem(14, 'Tempor magna ad consectetur', './img/water-house-min.jpg', 'Water house', 400, 250),
+    new GoodItem(
+      14,
+      'Tempor magna ad consectetur',
+      './img/water-house-min.jpg',
+      'Water house',
+      400,
+      250
+    ),
     new GoodItem(15, 'Reprehenderit', './img/bridge-min.jpg', 'Bridge', 400, 650),
     new GoodItem(16, 'Enim ipsum mollit', './img/hot-girl-min.jpg', 'China', 400, 750),
   ];
@@ -264,20 +276,29 @@ shopName = 'My Movie Shop';
     const parentSet = document.getElementById('gallery'); //   <ul></ul>
 
     for (const item of this.products) {
-    let renderedGood = new RenderedGood(item.id, item.link, item.thumb, item.image, item.alt, item.width, item.descr, item.title, item.price, item.btn);
-    
-    console.log(renderedGood);
-    const renderedElement = renderedGood.render();
-     
-    parentSet.appendChild(renderedElement);  // ul > li...
-    }
+      let renderedGood = new RenderedGood(
+        item.id,
+        item.link,
+        item.thumb,
+        item.image,
+        item.alt,
+        item.width,
+        item.descr,
+        item.title,
+        item.price,
+        item.btn
+      );
 
+      console.log(renderedGood);
+      const renderedElement = renderedGood.render();
+
+      parentSet.appendChild(renderedElement); // ul > li...
+    }
   }
 }
 
 class RenderedGood extends Component {
-
-    constructor(id, a, thumb, image, alt, width, descr, title, price, btn){
+  constructor(id, a, thumb, image, alt, width, descr, title, price, btn) {
     super('gallery');
 
     this.id = id;
@@ -290,73 +311,79 @@ class RenderedGood extends Component {
     this.title = title;
     this.price = price;
     this.btn = btn;
-    
-    }
- render(){
-//  <li>...</li>
-let element = this.createRenderedElement('li', 'gallery__item', [{name:'id', value:'good-' + this.id }]);
-let itemLink = this.createRenderedElement('a', 'gallery__item-link', [{name:'href', value:'#'}]);
-let thumbImg = this.createRenderedElement('div', 'gallery__item-thumb');
-let img = this.createRenderedElement('img', 'gallery__item-img', [{name:'src', value: this.image }, {name:'alt', value: this.alt }, {name:'width', value: this.width } ]);
-let elementBlock = this.createRenderedElement('div', 'gallery__item-descr');
-let title = this.createRenderedElement('h2', 'gallery__item-title title', [], this.title);
-let price = this.createRenderedElement('p', 'gallery__item-price', [], this.price + '$');
-let button = this.createRenderedElement('button', 'buy-btn btn', [], 'Buy');
-button.addEventListener('click', this.addGoodToCart.bind(this));
+  }
+  render() {
+    //  <li>...</li>
+    let element = this.createRenderedElement('li', 'gallery__item', [
+      { name: 'id', value: 'good-' + this.id },
+    ]);
+    let itemLink = this.createRenderedElement('a', 'gallery__item-link', [
+      { name: 'href', value: '#' },
+    ]);
+    let thumbImg = this.createRenderedElement('div', 'gallery__item-thumb');
+    let img = this.createRenderedElement('img', 'gallery__item-img', [
+      { name: 'src', value: this.image },
+      { name: 'alt', value: this.alt },
+      { name: 'width', value: this.width },
+    ]);
+    let elementBlock = this.createRenderedElement('div', 'gallery__item-descr');
+    let title = this.createRenderedElement('h2', 'gallery__item-title title', [], this.title);
+    let price = this.createRenderedElement('p', 'gallery__item-price', [], this.price + '$');
+    let button = this.createRenderedElement('button', 'buy-btn btn', [], 'Buy');
+    button.addEventListener('click', this.addGoodToCart.bind(this));
 
-//   li > img + div > title + price + btn
-thumbImg.appendChild(img);
+    //   li > img + div > title + price + btn
+    thumbImg.appendChild(img);
 
-elementBlock.appendChild(title);
-elementBlock.appendChild(price);
-elementBlock.appendChild(button);
+    elementBlock.appendChild(title);
+    elementBlock.appendChild(price);
+    elementBlock.appendChild(button);
 
-itemLink.appendChild(thumbImg);
-itemLink.appendChild(elementBlock);
-element.appendChild(itemLink);
+    itemLink.appendChild(thumbImg);
+    itemLink.appendChild(elementBlock);
+    element.appendChild(itemLink);
 
-return element;
-}
+    return element;
+  }
 
-addGoodToCart(){
-Shop.cart.updateCart(this);
-}
-
+  addGoodToCart() {
+    Shop.cart.updateCart(this);
+  }
 }
 
 class Cart {
-
-  #personalInformation = "00001"; // # before => for doesn`t show to console, will be underfined
-    constructor(){
+  #personalInformation = '00001'; // # before => for doesn`t show to console, will be underfined
+  constructor() {
     this.goodsInCart = [];
-    }
+  }
 
-get totalPrice(){
-  const sum = this.goodsInCart.reduce((prevValue, currentItem) => {return prevValue + currentItem.price}, 0); 
-  return sum;
-}
-    updateCart(good){
+  get totalPrice() {
+    const sum = this.goodsInCart.reduce((prevValue, currentItem) => {
+      return prevValue + currentItem.price;
+    }, 0);
+    return sum;
+  }
+  updateCart(good) {
     this.goodsInCart.push(good);
-   
+
     let cartElement = document.getElementById('cart');
 
     cartElement.querySelector('.goods-in-cart span').innerHTML = this.goodsInCart.length;
     cartElement.querySelector('.total-price span').innerHTML = this.totalPrice;
 
     console.log(this);
-    }
+  }
 }
 
-class Shop{
-static goodList;
-static cart;
+class Shop {
+  static goodList;
+  static cart;
 
-static init (){
-  this.goodList = new GoodList();
-  this.cart = new Cart();
-  this.goodList.render();
+  static init() {
+    this.goodList = new GoodList();
+    this.cart = new Cart();
+    this.goodList.render();
+  }
 }
-}
-
 
 Shop.init();
